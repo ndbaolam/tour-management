d743397f34cc42a2569ca1e431d5762b0e98734b
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-04-07 21:15:42 +0700

    Slide

--- public/client/js/script.js
+++ public/client/js/script.js
@@ -1,1 +1,16 @@
-console.log('hello');
+//Slide Tour Detail
+var swiperSliderThumb = new Swiper(".swiperSliderThumb", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+  });
+  var swiperSliderMain = new Swiper(".swiperSliderMain", {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiperSliderThumb,
+    },
+});
+//End Slide Tour Detail
