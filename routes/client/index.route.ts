0b058bc3c1d6253483fdf921860948172785d8bf
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-04-07 22:09:03 +0700

    Làm giao diện

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -2,6 +2,7 @@ import { Express } from "express";
 import { tourRoutes } from "./tour.route";
 import { categoryRoutes } from "./category.route";
 import { userRoutes } from "./user.route";
+import { cartRoutes } from "./cart.route";
 
 const clientRoutes = (app: Express): void => {
     app.use("/tours", tourRoutes);
@@ -9,6 +10,8 @@ const clientRoutes = (app: Express): void => {
     app.use("/categories", categoryRoutes);
 
     app.use('/user', userRoutes);
+
+    app.use(`/cart`, cartRoutes);
 }
 
 export default clientRoutes;
