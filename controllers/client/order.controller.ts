c25833f90b89c085c11932075e0c569afa272296
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-04-16 02:16:02 +0700

    Save order to database

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -1,11 +1,35 @@
 import { Request, Response } from "express";
+import Order from "../../models/order.model";
+
+import * as generateHelper from '../../helpers/generate.helper';
 
 //[POST] /order
 export const order = async (req: Request, res: Response) => {
   const data = req.body;
 
+  const dataOrder = {
+    code: "",
+    fullName: data.info.fullName,
+    phone: data.info.phone,
+    note: data.info.note,
+    status: "initial",
+  };
+
+  const order = await Order.create(dataOrder);
+  const orderId = order.dataValues.id;
+  const code = generateHelper.generateOrderCode(orderId);
+
+  await Order.update({
+    code: code
+  }, {
+    where: {
+      id: orderId
+    }
+  });
+
   res.json({
-    data: data,
+    code: 200,
+    orderCode: code,
     message: "Order Successfull!"
   });
 }
--- helpers/generate.helper.ts
+++ helpers/generate.helper.ts
@@ -23,3 +23,8 @@ export const generateRandomNumber = (length: number): string => {
   return result;
 };
 
+// Generate Mã đơn hàng
+export const generateOrderCode = (number: number): string => {
+  const code = `OD${String(number).padStart(8, '0')}`;
+  return code;
+};
